86d0279cea39e04786d827609c2436890f2a9b3a
Joel <joel.balmer@gmail.com>
2018-06-05 23:29:43 +0100

    changed libraries to babylonjs for the all pwoerful scene rendering

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-	type = "canvas";
-}
+window.addEventListener('DOMContentLoaded', function() {
+	var canvas = document.getElementById('renderCanvas');
+	var engine = new BABYLON.Engine(canvas, true);
+	var scene = createScene(engine, canvas);
 
-PIXI.utils.sayHello(type);
+	engine.runRenderLoop(function(){
+        scene.render();
+    });
+
+    window.addEventListener('resize', function(){
+        engine.resize();
+    });
+});
+
+var createScene = function(engine, canvas) {
+    // Create the scene space
+    var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+
+    // Add a camera to the scene and attach it to the canvas
+    var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas, true);
+
+    // Add lights to the scene
+    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+    // This is where you create and manipulate meshes
+	var box = BABYLON.MeshBuilder.CreateBox("box", {
+		height: 0.5,
+		width: 0.5,
+		depth: 0.5,
+	}, scene);
+
+    return scene;
+}
