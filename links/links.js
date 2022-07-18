3e1e234e7f7bfb53af4c721148dfc036e0805315
joel.balmer <Joel.Balmer@intoglobal.com>
2022-07-18 19:56:46 +0100

    fixed styling

---
+++ links/links.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+  let magicGrid = new MagicGrid({
+    container: "#links-container",
+    static: true,
+    animate: true
+  });
+
+  magicGrid.listen();
+  magicGrid.positionItems();
+
+  // Animate elements
+  let hr = document.getElementsByTagName('hr')[0];
+  hr.classList.add('animate-width', 'animate-fadein');
+
+  let container = document.getElementById('links-container');
+  setTimeout(() => container.classList.add('animate-fadein'), 500);
+});
